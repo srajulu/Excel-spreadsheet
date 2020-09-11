5bf21d5f8930f4d5a25d12b94b4b005bbf043109
SWETHA RAJULU <55911880+srajulu@users.noreply.github.com>
2020-09-11 21:25:10 +0530

    Update file.py

--- file.py
+++ file.py
@@ -2,3 +2,10 @@ import openpyxl as xl
 
 
 from openpyxl.chart import BarChart, Reference
+
+
+def process_workbook(filename):
+	wb = xl.loadworkbook(filename)
+
+	sheet = wb['Sheet1']
+	#cell = sheet['a1']
