1541de64126fa55ace9216acfab5a66c71934d5f
SWETHA RAJULU <55911880+srajulu@users.noreply.github.com>
2020-09-11 21:25:34 +0530

    Update file.py

--- file.py
+++ file.py
@@ -6,6 +6,8 @@ from openpyxl.chart import BarChart, Reference
 
 def process_workbook(filename):
 	wb = xl.loadworkbook(filename)
-
 	sheet = wb['Sheet1']
 	#cell = sheet['a1']
+	cell = sheet.cell(1, 1)
+	#print(cell.value)
+	#print(sheet.max_row)
