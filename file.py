6a178aab3eb41f32a35c443934456932e1bc81e5
SWETHA RAJULU <55911880+srajulu@users.noreply.github.com>
2020-09-11 21:25:52 +0530

    Update file.py

--- file.py
+++ file.py
@@ -11,3 +11,10 @@ def process_workbook(filename):
 	cell = sheet.cell(1, 1)
 	#print(cell.value)
 	#print(sheet.max_row)
+
+	for row in range(2, sheet.max_row + 1):
+		cell = sheet.cell(row, 3)
+		print(cell.value)
+		corrected_price = cell.value * 0.9
+		corrected_price_cell = sheet.cell(row, 4)
+		corrected_price_cell.value = corrected_price
